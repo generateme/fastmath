83932ca2e9b7ccd883a0125c1959542e3c3c5961
GenerateMe <generateme.blog@gmail.com>
2023-05-10 16:41:39 +0200

    v bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject generateme/fastmath "2.2.1-SNAPSHOT"
+(defproject generateme/fastmath "2.2.1"
   :description "Fast and primitive math library"
   :url "https://github.com/generateme/fastmath"
   :license {:name "The MIT Licence"
